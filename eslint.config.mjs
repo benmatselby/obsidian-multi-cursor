1338f39081ea02af0169a4d9969bcd1cf892c35d
Ben Selby <benmatselby@gmail.com>
2024-08-13 14:44:11 +0100

    Migrate to ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'no-redeclare': 'error',
+    },
+  },
+];
